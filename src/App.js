905defc72bb4aeff752a6447361a6ee356e90c1d
Kameshwar Das <kameshwardas44@gmail.com>
2023-04-02 01:45:31 +0530

    db json Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Contact from './Components/Pages/Contact';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Components/Pages/About';
+import NotFound from './Components/Pages/NotFound';
+import AddUser from './Components/Users/AddUser';
+import EditUser from './Components/Users/EditUser';
+import ViewUsers from './Components/Users/ViewUsers';
+import Signup from './Components/Signup/Signup';
+import Login from './Components/Signup/Login';
+import House from './Components/Pages/House';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+      <Route exact path= '/' element={<House/>} >House</Route>
+        <Route exact path='/About' element={<About/>}  >About</Route>
+        <Route exact path='/Contact' element={<Contact/>}  >Contact</Route>
+        <Route exact path='/users/add' element={<AddUser/>}  >AddUser</Route>
+        <Route exact path='/users/edit/:id' element={<EditUser/>}  >Edit User</Route>
+        <Route exact path='/users/:id' element={<ViewUsers/>}  >View Users</Route>
+        <Route exact path='/Signup' element={<Signup/>}  >Signup</Route>
+        <Route exact path='/login' element={<Login/>}  >Login</Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
